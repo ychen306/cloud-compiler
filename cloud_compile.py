bbf69fa099f4688c712c2d9602494bbadd879b58
peytonshields <pshields@mit.edu>
2021-03-29 14:23:53 -0400

    added support for more command line options

--- cloud_compile.py
+++ cloud_compile.py
@@ -17,7 +17,7 @@ chunk_size = 64 * 1024
 #             yield chunk
 #             chunk = await f.read(chunk_size)
 
-async def post(output_path, file_name, compressed, llc_args, opt_args):
+async def post(output_path, file_name, compressed, use_clang, llc_args, opt_args, clang_args, target):
 
     with open(file_name, 'rb') as f:
         data = f.read()
@@ -25,7 +25,7 @@ async def post(output_path, file_name, compressed, llc_args, opt_args):
     if not data:
         return
 
-    file_name = file_name.split('/')[-1]
+    file_name = os.path.basename(file_name)
     file_name = file_name.split('.')[0]
 
     if compressed:
@@ -37,7 +37,10 @@ async def post(output_path, file_name, compressed, llc_args, opt_args):
         'compressed': compressed,
         'data': data,
         'llc_args': llc_args,
-        'opt_args': opt_args
+        'opt_args': opt_args,
+        'clang_args': clang_args,
+        'use_clang': use_clang,
+        'target': target
     })
     
 
@@ -54,7 +57,7 @@ async def post(output_path, file_name, compressed, llc_args, opt_args):
         print("Unable to post {} to lambda due to {}.".format(file_name, e.__class__))
 
 
-async def main(source, compressed, llc_args, opt_args):
+async def main(source, compressed, use_clang, out_dir, llc_args, opt_args, clang_args, target):
 
     if os.path.isfile(source):
         files = [source]
@@ -62,27 +65,32 @@ async def main(source, compressed, llc_args, opt_args):
     else:
         files = glob.glob(os.path.join(source, "*.bc"))
 
-    output_path = os.path.join(source, "lambda_output")
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-
-    await asyncio.gather(*[post(output_path, file_name, compressed, llc_args, opt_args) for file_name in files])
+    await asyncio.gather(*[post(out_dir, file_name, compressed, use_clang, llc_args, opt_args, clang_args, target) for file_name in files])
     print("Finished requests to lambda.")
 
 def checkParams():
     source = ''
+    out_dir = ''
     compressed = False
+    use_clang = False
     opt_args = ''
     llc_args = ''
+    clang_args = ''
+    target = ''
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--compressed', action='store_true')
+    parser.add_argument('-c', '--compressed', action='store_true', help="Flag set if .ll or .bc file compressed")
+    parser.add_argument('-uc', '--use_clang', action='store_true', help='Flag set builds the object with clang else opt and llc')
+    parser.add_argument('-o', '--output', type=str, help="Output directory")
     parser.add_argument('-llc', '--llc_args', type=str, help="Comma separated flags to send to llc")
     parser.add_argument('-opt', '--opt_args', type=str, help="Comma separated flags to send to opt")
+    parser.add_argument('-clang', '--clang_args', type=str, help="Comma separated flags to send to clang")
+    parser.add_argument('-t', '--target', type=str, help="Target to build object for")
     parser.add_argument('file', help="File to compile (.bc or .ll)")
 
     args = parser.parse_args()
     compressed = args.compressed
+    use_clang = args.use_clang
 
     try:
         if args.llc_args:
@@ -93,14 +101,25 @@ def checkParams():
             opt_args = args.opt_args.split(",")
             opt_args = ['-' + arg for arg in opt_args]
             opt_args = ' '.join(opt_args)
+        if args.clang_args:
+            clang_args = args.clang_args.split(",")
+            clang_args = ['-' + arg for arg in clang_args]
+            clang_args = ' '.join(clang_args) 
 
+        if args.output:
+            out_dir = args.output
+        else:
+            out_dir = os.path.dirname(os.path.realpath(args.file))
+
+        target = args.target
         source = args.file
     except:
         print('Error parsing arguments')
         sys.exit(2)
 
-    return source, compressed, llc_args, opt_args
+
+    return source, compressed, use_clang, out_dir, llc_args, opt_args, clang_args, target
 
 if __name__ == "__main__":
-    source, compressed, llc_args, opt_args = checkParams()
-    asyncio.run(main(source, compressed, llc_args, opt_args))
+    source, compressed, use_clang, out_dir, llc_args, opt_args, clang_args, target = checkParams()
+    asyncio.run(main(source, compressed, use_clang, out_dir, llc_args, opt_args, clang_args, target))
